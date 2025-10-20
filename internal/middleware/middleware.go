6d52d2e6665138368ff9491a8b20021d82dfb5d7
Seb0927 <sebastianidrobo@hotmail.com>
2025-10-20 13:33:21 -0500

    fix: add * domain to access-control-allow-origin

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -46,7 +46,7 @@ func Logger(logger *zap.Logger) gin.HandlerFunc {
 // CORS returns a middleware that adds CORS headers
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "127.0.0.1:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
